a5ed4f96c67d2add54c53f56920d5aaee5db7120
fcl564764633 <564764633@qq.com>
2020-05-15 17:04:15 +0800

    配置中心

---
+++ cloud-config-client-3355/src/main/java/com/fcl/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,18 @@
+package com.fcl.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: FCL
+ * @DateTime: 2020/5/15 16:26
+ * @Description: TODO
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class,args);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/com/fcl/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,21 @@
+package com.fcl.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: FCL
+ * @DateTime: 2020/5/15 16:32
+ * @Description: TODO
+ */
+@RestController
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
